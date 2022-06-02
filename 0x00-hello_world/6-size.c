1c32769ddfcf08f5d4854b9eba30cc60b6eb2eee
abalha <abalha13@gmail.com>
2022-06-02 16:48:59 -0700

    script that generates the assembly code (Intel syntax) of a C code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+
+/**
+ * main - print size of different data type
+ * 
+ *  Return: Always 0 (Success) 
+ */
+
 int main(void)
 {
 printf("Size of a char: %i byte(s)\n", (int) sizeof(char));
