6be96cc26461d73d26921b927e8c4c6eedf4502c
abalha <abalha13@gmail.com>
2022-06-03 14:12:30 -0700

    print sinle digit 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int number = 0;
+
+	while (number <= 10)
+	{
+		print("%d", number);
+
+		++number;
+	}
+	putchar('\n');
+
+	return (0);
+}
