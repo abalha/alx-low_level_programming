a27fa58706222617662f846daa757f6cc7b4950f
abalha <abalha13@gmail.com>
2022-06-03 06:26:42 -0700

    correction of 101-quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * main - Wows
+ *
+ * Return: Always 1 (Error)
  */
+#include<stdio.h>
+#include <unistd.h>
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, \
+2015-10-19\n", 59);
+return (1);
 }
