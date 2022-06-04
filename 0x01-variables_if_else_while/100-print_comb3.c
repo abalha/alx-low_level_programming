3372515dbc44ce0c47ac23b4dc4188dabc7ab587
abalha <abalha13@gmail.com>
2022-06-03 23:03:20 -0700

    combination of two digts

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -starting point for this project all code are in main
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; ++i)
+	{
+	for (j = 0; j <= 0; ++j)
+	{
+		if (i >= j)
+		{
+			continue;
+		}
+			putchar(i);
+			putchar(j);
+			putchar(',');
+			putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
