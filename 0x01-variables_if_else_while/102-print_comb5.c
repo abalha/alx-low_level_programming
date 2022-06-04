64c3053e9267cc6820eddb1a23128e56716b562d
abalha <abalha13@gmail.com>
2022-06-04 09:26:27 -0700

    combination of two two digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * main - the starting point of program
+ *
+ * for this project code are written in main function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 99; ++i)
+	{
+	for (j = 0; j < 99; ++j)
+	{
+	if ((i == 0 && j != 0) || (j > i))
+	{
+	putchar(i / 10 + '0');
+	putchar(i % 10 + '0');
+	putchar(' ');
+	putchar(j / 10 + '0');
+	putchar(j % 10 + '0');
+	if (i < 98)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
