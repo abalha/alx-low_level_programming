5ec12bf97553d2bb6d77d6d19092e03363b85beb
abalha <abalha13@gmail.com>
2022-06-04 09:34:52 -0700

    combination of two two digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -24,11 +24,10 @@ int main(void)
 	putchar(' ');
 	putchar(j / 10 + '0');
 	putchar(j % 10 + '0');
-	if (i < 98)
-	{
+	if (i == 98 && j == 99)
+	continue;
 	putchar(',');
 	putchar(' ');
-	}
 	}
 	}
 	}
