bebe0f3b3b85f31d7663e365f73c34b9dafd0ecc
abalha <abalha13@gmail.com>
2022-06-07 15:32:25 -0700

    print alpgabet im smll letter

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - starting point
+ *
+ *Return: 0 (succesful)
+ */
+
+void print_alphabet(void)
+{
+char ch;
+
+	for  (ch = 'a'; ch <= 'z'; ++ch)
+	{
+		_putchar(ch);
+	}
+
+	_putchar('\n');
+}
