4d575966c3022576d795133799defa726f97e399
abalha <abalha13@gmail.com>
2022-06-07 14:28:07 -0700

    function nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - starting of the program
+ *
+ * Return: (0) if succesfull
+ */
+
+int main(void)
+{
+int count;
+char arr[9];
+
+	for (count = 0; count <= 9; ++count)
+	{
+		_putchar(arr[count]);
+		_putchar('/n');
+	}
+
+	return (0);
+}
