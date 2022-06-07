81e0ebd8b1e0a93e91fb6b62f51974116c222872
abalha <abalha13@gmail.com>
2022-06-07 14:50:13 -0700

    1st task print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,14 @@
 
 int main(void)
 {
-int count;
-char arr[8] = "_putchar";
-
-	for (count = 0; count <= 8; count++)
-	{
-		_putchar(arr[count]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
 	_putchar('\n');
 	return (0);
