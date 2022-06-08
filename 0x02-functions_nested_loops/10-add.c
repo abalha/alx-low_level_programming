f18047852ec8fcef4cef192ca928ff2ed29f582d
abalha <abalha13@gmail.com>
2022-06-08 07:50:22 -0700

    summabtion of two number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - check summation of two number
+ * @num1: inputed number
+ * @num2: inputed number
+ * Return: Always 0.
+ */
+int add(int num1, int num2)
+{
+
+	return (num1 + num2);
+}
