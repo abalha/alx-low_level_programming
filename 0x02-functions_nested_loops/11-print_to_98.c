daf0ba0bd7a5a082c9fae05cf327847f5f2a1afc
abalha <abalha13@gmail.com>
2022-06-08 09:37:04 -0700

    natural number to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@ void print_to_98(int n)
 	if (n <= 98)
 	{
 	do	{
-		printf("%d ", n);
+		printf("%d", n);
 		if (n != 98)
 		printf(", ");
 		n++;
@@ -28,4 +28,5 @@ void print_to_98(int n)
 	}
 		while (n >= 98);
 	}
+	printf("\n");
 }
