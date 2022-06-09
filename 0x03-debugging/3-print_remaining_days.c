e8b4997961ae070cdc832989f5c05b89867dd231
abalha <abalha13@gmail.com>
2022-06-09 06:14:58 -0700

    leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -3,10 +3,10 @@
 /**
  * print_remaining_days - takes a date and prints how many days are
  * left in the year, taking leap years into account
- * @month: shows month
- * @day: shows day
- * @year: save year
- * Return: function dosent rturn value
+ * @month: month in number format
+ * @day: day of month
+ * @year: year
+ * Return: void
  */
 
 void print_remaining_days(int month, int day, int year)
