25cb2edc0d5064c89fc535cbc19bd5dff1caa958
abalha <abalha13@gmail.com>
2022-06-09 17:53:30 -0700

    diagnol

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagonal - Draws a diagonal line using the \ character.
+ * @size : The number of \ characters to be printed.
+ */
+void print_diagonal(int size)
+{
+	int row, ds;
+
+	if (size > 0)
+	{
+		for (row = 0; row < size; ++row)
+		{
+			for (ds = 0; ds < row; ++ds)
+				_putchar(' ');
+			_putchar('\\');
+
+			if (len == size - 1)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
