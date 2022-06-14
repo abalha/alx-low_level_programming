c1303289873152fac54829fd914cea3f3ea8f1b2
abalha <abalha13@gmail.com>
2022-06-14 11:29:32 -0700

    crack password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,20 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /**
- * create password by
+ * main - random password generator for 101-crackme
  *
- * randpm number generator
+ * Return: always 0
  *
  */
 
 int main(void)
 {
-	int i;
+	int i, j, k, s;
+	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char p[58];
 
-	for (i = 0; i <5; i++)  
+	srand(time(NULL));
+	while (s != 2772)
 	{
-		printf(" %d", rand());
+		i = k = s = 0;
+		while ((2772 - 122) > s)
+		{
+			j = rand() % 62;
+			p[i] = c[j];
+			s += c[j];
+			i++;
+		}
+		while (c[k])
+		{
+			if (c[k] == (2772 - s))
+			{
+				p[i] = c[k];
+				s += c[k];
+				i++;
+				break;
+			}
+			k++;
+		}
 	}
-	return 0;  
-}  
+	p[i] = '\0';
+	printf("%s", p);
+	return (0);
+}
