7d80f9dbbedf4a33d79df920777abf06ad48adcd
abalha <abalha13@gmail.com>
2022-06-14 09:11:20 -0700

    random valid pasword

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create password by
+ *
+ * randpm number generator
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <5; i++)  
+	{
+		printf(" %d", rand());
+	}
+	return 0;  
+}  
