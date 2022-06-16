ed4aaea9bc34b768f34b619e260c9d2708c8a339
abalha <abalha13@gmail.com>
2022-06-16 06:36:12 -0700

    string concatenation2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strncpy - Funtion concatenating string.
+ * @dest: destination value.
+ * @src: source value.
+ * @n: Number bytes
+ * Return: dest.
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, src_lnt, last;
+
+	src_lnt = 0;
+	for (i = 0; src[i] != '\0'; i++)
+		src_lnt++;
+
+	if (n > src_lnt)
+		last = src_lnt;
+	else
+		last = n;
+
+	for (i = 0; i < last; i++)
+		dest[i] = src[i];
+
+	if (n > src_lnt)
+	{
+		for (i = src_lnt; i < n; i++)
+			dest[i] = '\0';
+	}
+
+	return (dest);
+}
