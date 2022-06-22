84f5a71f275689bc916c981cc93f59289a06162b
abalha <abalha13@gmail.com>
2022-06-22 13:04:25 -0700

    function to find sqrt of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,25 +1,32 @@
 #include "main.h"
 /**
- * _sqrt_recursion - function that returns the natural square root of a number
- * @n: number to find squareroot.
- * Return: the square root of number n
- */
+  * _sqrt_recursion - function that calculates the sqrt of a number
+  * @n: number to be calculated
+  * Return: sqrt of number
+  */
 int _sqrt_recursion(int n)
 {
-	return (test(1, n));
-}
 
+	return (find_sqrt(0, n));
+
+}
 /**
- * checker - checks the input number from n to the base
- * @n: number to be tested
- * @base: base number
- * Return: root of number base
- */
-int test(int n, int base)
+  * find_sqrt - Calculates the sqrt of a number
+  * @num: scale
+  * @base: number to check
+  * Return: -1.
+  */
+
+int find_sqrt(int num, int base)
 {
-	if (n * n == base)
-	return (n);
-	if (n * n > base)
-	return (-1);
-	return (test(n + 1, base));
+
+	if (num * num == base)
+		return (i);
+	else if (num < base)
+		{
+		return (find_sqrt(num + 1, base));
+		}
+	else
+		return (-1);
+
 }
